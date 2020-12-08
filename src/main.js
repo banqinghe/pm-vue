a545446da3fd9d034d21949d73f5aa652d894537
banqinghe <qingheban@qq.com>
2020-12-08 13:30:06 +0800

    init axios

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+axios.defaults.baseURL = '/api'
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset=UTF-8' 
+
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
